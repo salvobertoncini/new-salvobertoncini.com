4186353ee5e09670c56501e69d81f64296fe429d
Salvo Bertoncini <salvobertoncini@users.noreply.github.com>
2017-08-05 17:02:45 +0200

    home ok, splitted sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 import { AppContainerComponent } from './app-container/app-container.component';
 import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppSidebarComponent } from './app-sidebar/app-sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AppNavbarComponent,
     AppContainerComponent,
-    AppFooterComponent
+    AppFooterComponent,
+    AppSidebarComponent
   ],
   imports: [
     BrowserModule
